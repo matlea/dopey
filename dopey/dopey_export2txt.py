7ce778c240402c8dd64568ecde1dcb866865a41e
MatLea <167424221+matlea@users.noreply.github.com>
2024-11-30 16:36:20 +0100

    Update 2024.11.30

--- dopey/dopey_export2txt.py
+++ dopey/dopey_export2txt.py
@@ -1,5 +1,5 @@
 
-__version__ = "24.05.28"
+__version__ = "24.11.30"
 __author__  = "Mats Leandersson"
 
 print(f"{__name__}, {__version__}")
@@ -32,7 +32,8 @@ def export2txt(D = {}, fn = None, shup = False, **kwargs):
     if TYPE == "arpes": _export2txt_ARPES(D = D, fn = fn, shup = shup, **kwargs)
     elif TYPE == "spin_edc": _export2txt_SpinEDC(D = D, fn = fn, shup = shup, **kwargs)
     elif TYPE == "spin_mdc" and D.get("experiment", {}).get("Scan_Mode", "") == "FixedEnergies": _export2txt_SpinMDC_FE(D = D, fn = fn, shup = shup, **kwargs)
-    #elif TYPE == "spin_mdc" and D.get("experiment", {}).get("Scan_Mode", "") == "FixedAnalyzerTransmission": _save2txt_SpinMDC_FAT(D = D, fn = fn, shup = shup, **kwargs)  # One version
+    elif TYPE == "spin_mdc" and D.get("experiment", {}).get("Scan_Mode", "") == "FixedAnalyzerTransmission": _export2txt_SpinMDC_FAT(D = D, fn = fn, shup = shup, **kwargs)
+    elif TYPE == "spin_map" and D.get("experiment", {}).get("Scan_Mode", "") == "FixedEnergies": _export2txt_SpinMap_FE(D = D, fn = fn, shup = shup, **kwargs)
     else:
         print(Fore.MAGENTA + "export2txt(): The method is not ready for this data type. A work in progress. The different types are added one by one." + Fore.RESET)
 
@@ -84,13 +85,15 @@ def _export2txt_ARPES(D = {}, fn = "", shup = False, **kwargs):
 def _export2txt_SpinEDC(D = {}, fn = "", shup = False, **kwargs):
     """
     """
+    accepted_kwargs = ["data"]
+    accepted_data = ["scans", "mean"]
     if not shup:
-        print(Fore.BLUE + 'export2txt(): Spin EDC data is saved as columns, the first column being the energy values.' + Fore.RESET)
-        print(Fore.BLUE + '              To save the mean intenities pass argument data = "mean", and to save' + Fore.RESET)
-        print(Fore.BLUE + '              all intenity curves pass data = "all" (default).' + Fore.RESET)
-    data = kwargs.get("data", "all").lower()
-    if not data in ["all", "mean"]:
-        print(Fore.RED + 'export2txt(): Unknown value for the format keyword argument. Setting it to default "all".' + Fore.RESET)
+        print(Fore.BLUE + f'export2txt(): Accepted keyword arguments are {accepted_kwargs}.' + Fore.RESET)
+        print(Fore.BLUE + f'export2txt(): Accepted values for keyword argument data are {accepted_data}.' + Fore.RESET)
+    #
+    data = kwargs.get("data", "scans").lower()
+    if not data in accepted_data:
+        print(Fore.RED + 'export2txt(): Unknown value for the keyword argument data. Setting it to default "scans".' + Fore.RESET)
         data = "all"
     #
     file = open(fn, "w")
@@ -100,9 +103,8 @@ def _export2txt_SpinEDC(D = {}, fn = "", shup = False, **kwargs):
     file.write(f'# Energy            : {D["experiment"]["Energy_Axis"]}\n')
     file.write(f'# Intensity         : {D["experiment"]["Count_Rate"]}\n')
     n = np.shape(D["intensity"])[1]
-    file.write(f'# EDCs_per_polarity : {n}\n')
     #
-    if data == "all": 
+    if data == "scans": 
         file.write("#\n# data              : intensity per scan\n")
         file.write(f"# columns           : energy, {n} x negative polarity, {n} x positive polarity\n")
     else: 
@@ -111,9 +113,9 @@ def _export2txt_SpinEDC(D = {}, fn = "", shup = False, **kwargs):
     #
     for i, energy in enumerate(D["x"]):
         row = f"{energy:7.3f}"
-        if data == "average":
+        if data == "mean":
             row = f'{row}\t{D["intensity_avg"][0][i]:.5e}\t{D["intensity_avg"][0][i]:.5e}\n'
-        elif data == "all":
+        elif data == "scans":
             for j in range(n):
                 row = f'{row}\t{D["intensity"][0][j][i]:.5e}'
             for j in range(n):
@@ -129,14 +131,17 @@ def _export2txt_SpinEDC(D = {}, fn = "", shup = False, **kwargs):
 def _export2txt_SpinMDC_FE(D = {}, fn = "", shup = False, **kwargs):  # Note: this is almost identical to _save2txt_SpinEDC().
     """
     """
+    accepted_kwargs = ["data"]
+    accepted_data = ["scans", "mean"]
     if not shup:
-        print(Fore.BLUE + 'export2txt(): Spin MDC data is saved as columns, the first column being the energy values.' + Fore.RESET)
-        print(Fore.BLUE + '              To save the average intenities pass argument data = "average", and to save' + Fore.RESET)
-        print(Fore.BLUE + '              all intenity curves pass data = "all" (default).' + Fore.RESET)
-    data = kwargs.get("data", "all").lower()
-    if not data in ["all", "average"]:
-        print(Fore.RED + 'export2txt(): Unknown value for the format keyword argument. Setting it to default "all".' + Fore.RESET)
-        data = "all"
+        print(Fore.BLUE + f'export2txt(): Accepted keyword arguments are {accepted_kwargs}.' + Fore.RESET)
+        print(Fore.BLUE + f'export2txt(): Accepted values for keyword argument data are {accepted_data}.' + Fore.RESET)
+    #
+
+    data = kwargs.get("data", "mean").lower()
+    if not data in accepted_data:
+        print(Fore.RED + 'export2txt(): Unknown value for the keyword argument data. Setting it to default "mean".' + Fore.RESET)
+        data = "mean"
     #
     NE = kwargs.get("NE", 0)  # This is used by _save2txt_SpinMDC_FAT() to select which energy to save.
     #
@@ -144,13 +149,14 @@ def _export2txt_SpinMDC_FE(D = {}, fn = "", shup = False, **kwargs):  # Note: th
     file.write(f'# Type              : {D["type"]}\n')
     experiment_keys = ["Spectrum_ID", "Lens_Mode", "Scan_Mode", "Ep", "Excitation_Energy", "Dwell_Time"]
     for key in experiment_keys: file.write(f'# {key:<18}: {D["experiment"][key]}\n')
-    file.write(f'# Energy            : {D["experiment"]["Energy_Axis"]}\n')
+    file.write(f'# Energy axis       : {D["experiment"]["Energy_Axis"]}\n')
     file.write(f'# Intensity         : {D["experiment"]["Count_Rate"]}\n')
     file.write(f'# Deflector         : {D["labels"]["y"]}\n')
+    file.write(f'# Energy            : {D["x"][0]}\n')
     n = np.shape(D["intensity"])[1]
     file.write(f'# MDCs_per_polarity : {n}\n')
     #
-    if data == "all": 
+    if data == "scans": 
         file.write("#\n# data              : intensity per scan\n")
         file.write(f"# columns           : deflector, {n} x negative polarity, {n} x positive polarity\n")
     else: 
@@ -158,15 +164,15 @@ def _export2txt_SpinMDC_FE(D = {}, fn = "", shup = False, **kwargs):  # Note: th
         file.write("# columns           : deflector, negative polarity, positive polarity\n")
     #
     for i, deflector in enumerate(D["y"]):
-        row = f"{deflector:7.3f}"
-        if data == "average":
-            row = f'{row}\t{D["intensity_mean"][0][i][NE]:.5e}\t{D["intensity_mean"][1][i][NE]:.5e}\n'
-        elif data == "all":
+        row = f"{deflector:6.2f} "
+        if data == "mean":
+            row = f'{row} {D["intensity_mean"][0][i][NE]:.5e} {D["intensity_mean"][1][i][NE]:.5e}\n'
+        elif data == "scans":
             for j in range(n):
-                row = f'{row}\t{D["intensity"][0][j][i][NE]:.5e}'
+                row = f'{row} {D["intensity"][0][j][i][NE]:.5e}'
             for j in range(n):
-                row = f'{row}\t{D["intensity"][1][j][i][NE]:.5e}'
-            row = f'{row}\n'
+                row = f'{row} {D["intensity"][1][j][i][NE]:.5e}'
+            row = row + "\n"
         file.write(row)
     #
     file.close()
@@ -177,6 +183,9 @@ def _export2txt_SpinMDC_FE(D = {}, fn = "", shup = False, **kwargs):  # Note: th
 def _export2txt_SpinMDC_FAT(D = {}, fn = "", shup = False, **kwargs):
     """
     """
+    #
+    print(Fore.LIGHTBLACK_EX + "export2txt(): This data format (spin MDC in Fixed Analyzer Transmission mode) is not yet ready for be written to text files." + Fore.RESET); return
+    #
     if not shup:
         print(Fore.BLUE + 'export2txt(): Spin MDC data is saved as columns, the first column being the energy values.' + Fore.RESET)
         print(Fore.BLUE + '              To save the average intenities pass argument data = "mean", and to save' + Fore.RESET)
@@ -194,3 +203,80 @@ def _export2txt_SpinMDC_FAT(D = {}, fn = "", shup = False, **kwargs):
         fn_ = f'{fn[:-4]}_E{i}={energy:.3f}eV{fn[-4:]}'
         _export2txt_SpinMDC_FE(D = D, fn = fn_, shup = True, data = data, NE = i)
     #
+
+
+
+def _export2txt_SpinMap_FE(D = {}, fn = "", shup = False, **kwargs):
+    """
+    """
+    #
+    accepted_kwargs = ["data"]
+    accepted_data = ["scans", "mean"]
+    if not shup:
+        print(Fore.BLUE + f'export2txt(): Accepted keyword arguments are {accepted_kwargs}.' + Fore.RESET)
+        print(Fore.BLUE + f'export2txt(): Accepted values for keyword argument data are {accepted_data}.' + Fore.RESET)
+    #
+    data = kwargs.get("data", "mean").lower()
+    if not data in accepted_data:
+        print(Fore.RED + f'export2txt(): Unknown value for the format keyword argument. Available values are: {accepted_data}. Setting it to default "scans".' + Fore.RESET)
+        data = "scans"
+    #
+    fn_base = fn.split(".")[0]
+    fn_defl1, fn_defl2 = f"{fn_base}_defl1.dat", f"{fn_base}_defl2.dat"
+    file1, file2 = open(fn_defl1, "w"), open(fn_defl2, "w")
+    file1.write(f'# Deflector         : {D["labels"]["y"]}\n')
+    file2.write(f'# Deflector         : {D["labels"]["z"]}\n')
+    for defl in D["y"]: file1.write(f"{defl:6.2f}\n")
+    for defl in D["z"]: file2.write(f"{defl:6.2f}\n")
+    file1.close(); file2.close()
+    if not shup:
+        print(Fore.BLUE + f'export2txt(): The deflector axes are written to files {fn_defl1} and {fn_defl2}.' + Fore.RESET)
+    #
+    fn_plus, fn_minus = f"{fn_base}_plus.dat", f"{fn_base}_minus.dat"
+    file1, file2 = open(fn_plus, "w"), open(fn_minus, "w")
+    for file in [file1, file2]:
+        file.write(f'# Type              : {D["type"]}\n')
+    file1.write(f'# Polarity          : minus\n'); file2.write(f'# Polarity          : minus\n')
+    for file in [file1, file2]:
+        experiment_keys = ["Spectrum_ID", "Lens_Mode", "Scan_Mode", "Ep", "Excitation_Energy", "Dwell_Time"]
+        for key in experiment_keys: file.write(f'# {key:<18}: {D["experiment"][key]}\n')
+        file.write(f'# Energy axis       : {D["experiment"]["Energy_Axis"]}\n')
+        file.write(f'# Intensity         : {D["experiment"]["Count_Rate"]}\n')
+        file.write(f'# Deflector1        : {D["labels"]["y"]}\n')
+        file.write(f'# Deflector2        : {D["labels"]["z"]}\n')
+        file.write(f'# Energy            : {D["x"][0]}\n')
+    #
+    if data == "mean":
+        for row in D["intensity_mean"][1]:
+            wrow = ""
+            for r in row: wrow += f"{r[0]:.5e}\t"
+            file1.write(wrow + "\n")
+        for row in D["intensity_mean"][0]:
+            wrow = ""
+            for r in row: wrow += f"{r[0]:.5e}\t"
+            file2.write(wrow + "\n")
+        if not shup:
+            print(Fore.BLUE + f'export2txt(): The mean intensities are written to files {fn_plus} and {fn_minus}.' + Fore.RESET)
+    #
+    elif data == "scans":
+        for file in [file1, file2]: file.write("#\n")
+        for i, scan in enumerate(D["intensity"][1]):
+            file1.write(f"# Scan: {i}\n")
+            for row in scan:
+                wrow = ""
+                for r in row: wrow += f"{r[0]:.5e}\t"
+                file1.write(wrow + "\n")
+        for i, scan in enumerate(D["intensity"][0]):
+            file2.write(f"# Scan: {i}\n")
+            for row in scan:
+                wrow = ""
+                for r in row: wrow += f"{r[0]:.5e}\t"
+                file2.write(wrow + "\n")
+        if not shup:
+            print(Fore.BLUE + f'export2txt(): The intensities for the individual scans are written to files {fn_plus} and {fn_minus}.' + Fore.RESET)
+    #
+    file1.close(); file2.close()
+
+
+    
+    

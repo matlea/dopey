965e39027423c5069486db34f983f0a4c5a659e4
MatLea <167424221+matlea@users.noreply.github.com>
2024-12-03 15:20:11 +0100

    Updates 2024.12.03

--- dopey/__init__.py
+++ dopey/__init__.py
@@ -1,12 +1,11 @@
 __author__ = "Mats Leandersson"
-__version__ = "2024.11.28"
+__version__ = "2024.12.03"
 
 
 print(f"{__name__}, {__author__}")
 
 
 from colorama import Fore
-#print(Fore.LIGHTBLACK_EX + "Still translating grumpy into dopey so there might be some bugs or unfinished methods...\n" + Fore.RESET)
 
 try: from dopey.dopey_constants import *
 except Exception as E1: 
@@ -48,13 +47,7 @@ except Exception as E1:
     print(Fore.RED + "dopey: issue(s) with importing dopey_dichroism.py")
     print(E1, Fore.RESET)
 
-#
-print()
-#try:
-#    with open("dopey/dopey_help.txt", "r") as f:
-#        for row in f: print(row.strip("\n"))
-#except:
-#    pass
+
 
 
 
